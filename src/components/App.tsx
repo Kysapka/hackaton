52ff818084a5b81fcabd96f694448ca951c67be7
Damil <71599031+xLeeVeNx@users.noreply.github.com>
2021-09-25 00:02:17 +0600

    added promo page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Decor } from './Decor/Decor';
+import { StartButton } from './StartButton/StartButton';
 
 export const App = () => {
 	return (
-      <h1>Hackathon</h1>
+      <div className="wrapper">
+	      <Decor />
+	      <StartButton />
+      </div>
 	);
 };
---
+++ src/components/StartButton/StartButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const StartButton: React.FC = () => {
+	return (
+		<button className="start-button">Start Magic</button>
+	);
+};
