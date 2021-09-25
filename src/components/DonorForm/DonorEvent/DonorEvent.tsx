19a2344c54387d5e42c4d20d933d8fd6ed067ffb
Anjeli-art <shinyanj@gmail.com>
2021-09-25 07:46:01 +0300

    donor

---
+++ src/components/DonorForm/DonorEvent/DonorEvent.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {v1} from "uuid";
+
+type propsType = {
+    name: string
+    setName: (value:string) => void
+    radio: string
+    setRadio: (value:string) => void
+    formaEnter:boolean
+    setFormaEnter:(value:boolean)=>void
+}
+
+export const DonorEvent = (props:propsType) => {
+    return (
+        <div>
+            <form>
+                <p>Вы волшебник первого уровня,давайте познакомимся и вы
+                    выберите вид помощи который вы хотите оказать</p>
+                <div>
+                    <input type={"text"} value={props.name} onChange={(e) => {
+                        props.setName(e.currentTarget.value)
+                    }}/>
+                </div>
+                <div>
+                    <input type="radio" id="contactChoice1" name="contact" value="event" onChange={(e) => {
+                        props.setRadio(e.currentTarget.value)
+                    }}/>
+                    <label htmlFor="contactChoice1">Я хочу организовать мероприятие</label>
+                    <input type="radio" id="contactChoice2" name="contact" value="finance" onChange={(e) => {
+                        props.setRadio(e.currentTarget.value)
+                    }}/>
+                    <label htmlFor="contactChoice2">Оплачу курсы,куплю игрушки,помогу финансово</label>
+                    <input type="radio" id="contactChoice3" name="contact" value="private" onChange={(e) => {
+                        props.setRadio(e.currentTarget.value)
+                    }}/>
+                    <label htmlFor="contactChoice3">Свяжитесь со мной лично</label>
+                </div>
+                <div>
+                    <button type="submit" onClick={() => {
+                        props.setFormaEnter(true);
+                        // props.setUser([...users, {    ///////////dispatch
+                        //     id: v1(),
+                        //     name:props.name,
+                        //     formaEnter:props.formaEnter,
+                        //     radio: "event",
+                        //     formaContact: false,
+                        //     contact: "",
+                        //     rating: "",
+                        //     events: ""
+                        // }])
+                    }}>Выбрать вид помощи
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+DonorEvent;
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import './scss/main.scss';
 import {App} from './components/App';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import  {DonorForm} from "./components/DonorForm/DonorForm";
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <DonorForm/>
+            {/*<App/>*/}
         </BrowserRouter>
     </React.StrictMode>
     ,
