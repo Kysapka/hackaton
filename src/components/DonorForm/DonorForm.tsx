98eb40f7a87420ebeee7dd361b96c75dd58f8b3c
Артем <kysapka@gmail.com>
2021-09-25 20:31:07 +0300

    REDUX connected

--- src/components/DonorForm/DonorForm.tsx
+++ src/components/DonorForm/DonorForm.tsx
@@ -1,44 +1,31 @@
 import React, {useContext, useState} from 'react';
 import Rating from "../RaitingComponent/Rating";
-import {v1} from "uuid";
-import {initUserType} from "../../Store/Reducer_User";
-import {Donor, userType} from "./Donor/Donor";
+import {Donor} from "./Donor/Donor";
 import {DonorFinance} from "./DonorFinance/DonorFinance";
 import {Donorcontact} from "./DonorContact/Donorcontact";
 import {DonorEvent} from "./DonorEvent/DonorEvent";
-import {Context} from "../../index";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {useCollectionData} from "react-firebase-hooks/firestore";
-import firebase from "firebase/compat";
+import {useDispatch, useSelector} from "react-redux";
+import {RootStateType} from "../../Store/Store";
+import {addSponsorAC, sponsorsStateType} from "../../Store/Reducer_Sponsor";
 
 export const DonorForm = () => {
 
+    const dispatch = useDispatch()
+    let sponsors = useSelector<RootStateType, sponsorsStateType>(state => state.sponsors)
 
-    let [radio, setRadio] = useState("")
-    let [formaEnter, setFormaEnter] = useState(false)
-    let [user, setUser] = useState<userType>({name: "",password:"" ,formaEnter: false, radio: ''})
+    let [user, setUser] = useState<any>({})
+    let [formaEnter, setformaEnter] = useState(false)
 
-    // Firebase
-    const {auth, firestore} = useContext<any>(Context)
-    const [auth_user] = useAuthState(auth)
-    // const [startForm, loading] = useCollectionData<any>()
-
-    const addUser = async () => {
-        firestore().collection('users').add(
-            {
-                ...user,
-                createdAt: firebase.firestore.FieldValue.serverTimestamp()
-            }
-        )
+    const addUser = () => {
+        dispatch(addSponsorAC(user.name, true, user.radio ))
     }
 
-
     return <div>
-        {formaEnter || <Donor addUser={addUser} newUser={user} setNewUser={setUser} radio={radio} setRadio={setRadio}
-                              formaEnter={formaEnter} setFormaEnter={setFormaEnter}/>}
-        {radio === "private" && formaEnter && <Donorcontact/>}
-        {radio === "event" && formaEnter && <DonorEvent/>}
-        {radio === "finance" && formaEnter && <DonorFinance/>}
+        {formaEnter}{user.radio}
+        {formaEnter || <Donor addUser={addUser} newUser={user} setNewUser={setUser} setformaEnter={setformaEnter}/>}
+        {user.radio === "private" && formaEnter && <Donorcontact/>}
+        {user.radio === "event" &&   formaEnter && <DonorEvent/>}
+        {user.radio === "finance" && formaEnter && <DonorFinance/>}
         <Rating/>
     </div>
 }
